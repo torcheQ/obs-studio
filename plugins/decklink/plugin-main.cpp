bda9f8414c81b200ffc528154d00fd523c3171c8
Colin Edwards <colin@recursivepenguin.com>
2018-01-06 19:24:33 -0600

    Decklink out works on windows

--- plugins/decklink/plugin-main.cpp
+++ plugins/decklink/plugin-main.cpp
@@ -1,11 +1,5 @@
 #include <obs-module.h>
 
-#include <obs-frontend-api.h>
-#include <QAction>
-#include <QMainWindow>
-
-#include "DecklinkOutputUI.h"
-
 #include "decklink-devices.hpp"
 
 OBS_DECLARE_MODULE()
@@ -16,24 +10,6 @@ struct obs_source_info decklink_source_info;
 
 extern struct obs_output_info create_decklink_output_info();
 struct obs_output_info decklink_output_info;
-
-DecklinkOutputUI *doUI;
-
-void addOutputUI(void) {
-    QAction *action = (QAction*)obs_frontend_add_tools_menu_qaction(
-            obs_module_text("Decklink Outputs"));
-
-	QMainWindow *window = (QMainWindow*)obs_frontend_get_main_window();
-
-    doUI = new DecklinkOutputUI(window);
-
-    auto cb = [] ()
-    {
-        doUI->ShowHideDialog();
-    };
-
-    action->connect(action, &QAction::triggered, cb);
-}
 
 bool obs_module_load(void)
 {
@@ -46,8 +22,6 @@ bool obs_module_load(void)
 
 	decklink_output_info = create_decklink_output_info();
 	obs_register_output(&decklink_output_info);
-
-    addOutputUI();
 
 	return true;
 }
